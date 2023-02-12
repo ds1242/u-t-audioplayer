6b05b37e0194be13f62eb47096c96038e68c5afd
ds1242 <david.shaw1242@gmail.com>
2023-02-12 12:55:31 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+const interviewList = [
+    {
+        title: "Interview 1",
+        path: 'https://digital.lib.utk.edu/collections/islandora/object/wwiioh%3A2002/datastream/PROXY_MP3'
+    },
+    {
+        title: "Interview 2",
+        path: "https://digital.lib.utk.edu/collections/islandora/object/wwiioh%3A2003/datastream/PROXY_MP3"
+    }
+]
