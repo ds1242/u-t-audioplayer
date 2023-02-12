8f38ba70ad3d27aa3c1b1ab957c4bcc642e4347e
ds1242 <david.shaw1242@gmail.com>
2023-02-12 13:59:46 -0700

    added comments to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// create  interview array with objects for tracks
 const interviewList = [
     {
         title: "Interview 1",
@@ -8,7 +9,10 @@ const interviewList = [
         path: "https://digital.lib.utk.edu/collections/islandora/object/wwiioh%3A2003/datastream/PROXY_MP3"
     }
 ]
+// inital isPlaying set to false to used throughout
 let isPlaying = false;
+
+// pause and play function that operates the audio
 
 const pausePlay = () => {    
 
@@ -24,13 +28,13 @@ const pausePlay = () => {
             console.log(isPlaying)
         }
 }
-
+// back function resets timer to zero and adjusts pause play icons
 const back = () => {
     currentTrack.pause();
     currentTrack.currentTime = 0;
     setPausePlayIcon(isPlaying)
 }
-
+// function to check the current pause and play status
 const setPausePlayIcon = (isPlaying) => {
     if (isPlaying === false) {
         pausePlayIcon.classList.remove('bi-play');
@@ -41,14 +45,15 @@ const setPausePlayIcon = (isPlaying) => {
         pausePlayIcon.classList.add('bi-play');
     }
 }
-
+// create variables from DOM
 const playButton = document.getElementById('play-button');
 const pausePlayIcon = document.getElementById('play-icon');
 const backButton = document.getElementById('back-button');
 const skipButton = document.getElementById('skip-button');
+
 let currentTrack = new Audio(interviewList[0].path);
 
-
+// event listeners to execute functions
 playButton.addEventListener('click', pausePlay);
 backButton.addEventListener('click', back);
 backButton.addEventListener('click', skip);
